2a7bd66abf04c0320d6fb888befa13a0bdd117ce
Lucas Passos <lucas.depassos@yahoo.com.br>
2022-08-04 15:52:47 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+</script>
+
+<template>
+  <div>
+    <div class="bg-primary w-screen h-screen">
+      <div class="flex align-top justify-center w-[400px] h-[300px] m-auto">
+        <img src="./assets/char.gif" />
+      </div>
+    </div>
+  </div>
+ 
+</template>
---
+++ tailwind.config.cjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'primary': '#292524',
+      'secondary':'#5b21b6'
+      
+    },
+  },
+  plugins: [],
+}
