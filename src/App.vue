7816424c4a50bdc8d7d3b6d103fee0d9f0ac3ee0
Lucas Passos <lucas.depassos@yahoo.com.br>
2022-08-04 16:04:13 -0300

    added animation and front-page

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,11 @@
       <div class="flex align-top justify-center w-[400px] h-[300px] m-auto">
         <img src="./assets/char.gif" />
       </div>
+      <div>
+        <div class="bg-secondary w-[800px] h-[400px] opacity-20 border-4 border-indigo-200 border-l-[white] m-auto">
+          sssss
+        </div>
+      </div>
     </div>
   </div>
  
---
+++ src/components/StartGame.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <button class="bg-secondary border-[white] border-4">Começar</button>
+        <button class="bg-secondary border-[white] border-4">Regras</button>
+    </div>
+
+</template>
+
+<script>
+export default {
+    data: {
+        return: {
+
+        }
+    }
+}
+</script>
